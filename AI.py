c0bbdb4ae6e872f93f07f7659f744605a695baca
boybetby <boybetby@gmail.com>
2020-10-19 19:08:31 +0700

    add

--- AI.py
+++ AI.py
@@ -1,6 +1,9 @@
 import speech_recognition as sr
 import pyttsx3
+import subprocess
+import time
 from googlesearch import search 
+
 
 AI_hear = sr.Recognizer()
 AI_understand = ''
@@ -16,22 +19,29 @@ while running :
         print('AI: ...')
     try:        
         me = AI_hear.recognize_google(audio)
+        print("Me: "+me)
     except:
         me = ""
         
     #understand
     if me == '':
         AI_understand = 'Im listening'
-    elif 'hello' in me:
+    elif 'hello' in me.lower():
         AI_understand = 'Hello'   
-    elif 'stop' in me:
+    elif 'stop' in me.lower():
         running = False    
         AI_understand = 'See you later'
-    elif 'google' or 'search for' or 'find' in me:
+    elif 'steam' in me.lower():
+        print('AI: Opening Steam...')
+        subprocess.call('F:\Steam\Steam.exe')
+        print('Done')
+        time.sleep(2)
+    elif 'google' or 'search for' or 'find' in me.lower():
         print('Result for: ' +me)
         for i in search(me, tld='com', lang='en', num=10,stop=5, pause=2.0):   
             print(i) 
         me = ''
+        
     #speak 
     print('AI: '+AI_understand)      
     AI_speak.say(AI_understand)
---
+++ test.py
@@ -0,0 +1,2 @@
+from twill.commands import *
+go('https://www.youtube.com/')
